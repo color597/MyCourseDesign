5e65d4cfbaea53050be5fbbdb4e1e1c0bb1f747d
Col_or <color597@qq.com>
2023-06-21 15:53:53 +0800

    CourseDesign: Initial first commit

---
+++ src/main/kotlin/com/color597/course_design/CourseDesignApplication.kt
@@ -0,0 +1,11 @@
+package com.color597.course_design
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourseDesignApplication
+
+fun main(args: Array<String>) {
+    runApplication<CourseDesignApplication>(*args)
+}
---
+++ src/test/kotlin/com/color597/course_design/CourseDesignApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.color597.course_design
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseDesignApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
